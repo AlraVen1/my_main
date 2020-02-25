667a5b3dd2ee6c8ace0720c3ac101b7d9bbd61e4
Rafal Skrabalak <rafal.skrabalak@justpremium.com>
2020-02-25 14:53:30 +0100

    finished

---
+++ Fetch_exercises/main.js
@@ -0,0 +1,46 @@
+document.querySelector('#getText').addEventListener('click', getText);
+document.querySelector('#getUsers').addEventListener('click', getUsers);
+document.querySelector('#getPosts').addEventListener('click', getPosts);
+
+function getText(){
+    fetch('sample.txt')
+    .then((res) => res.text())
+    .then((data) => {
+        document.getElementById('output').innerHTML = data;
+    })
+}
+
+function getUsers(){
+    fetch('users.json')
+    .then((res) => res.json())
+    .then((data) => {
+        let output = '<h2>Users</h2>';
+        data.forEach(function(user){
+            output += `
+                <ul>
+                    <li>ID: ${user.id}</li>
+                    <li>Name: ${user.name}</li>
+                    <li>Email: ${user.email}</li>
+                </ul>
+            `;
+        });
+        document.getElementById('output').innerHTML = output;
+    })
+}
+
+function getPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((data) => {
+        let output = '<h2>Posts</h2>';
+        data.forEach(function(post){
+            output += `
+                <div>
+                    <h3>${post.title}</h3>
+                    <p>${post.body}</p>
+                </div>
+            `;
+        });
+        document.getElementById('output').innerHTML = output;
+    })
+}
