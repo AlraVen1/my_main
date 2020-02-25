294e1077108dc126292a319c39f34e7b75c45199
Rafal Skrabalak <rafal.skrabalak@justpremium.com>
2020-02-25 15:21:48 +0100

    added form

--- Fetch_exercises/main.js
+++ Fetch_exercises/main.js
@@ -1,6 +1,7 @@
 document.querySelector('#getText').addEventListener('click', getText);
 document.querySelector('#getUsers').addEventListener('click', getUsers);
 document.querySelector('#getPosts').addEventListener('click', getPosts);
+document.querySelector('#addPost').addEventListener('submit', addPost);
 
 function getText(){
     fetch('sample.txt')
@@ -43,4 +44,23 @@ function getPosts(){
         });
         document.getElementById('output').innerHTML = output;
     })
+}
+
+//to fix
+function addPost(e) {
+    e.preventDefault();
+
+    let title = document.querySelector('#title').value;
+    let body = document.querySelector('#body').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        },
+        body:JSON.stringify({title:title, body:body})
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
 }
